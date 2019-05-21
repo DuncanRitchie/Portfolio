28dd3acb075f81b4437466de968610c36c74af11
Duncan Ritchie <duncanritchie@btinternet.com>
2019-05-21 11:10:17 +0100

    Added .assign()

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 window.location.replace("https://www.duncanritchie.co.uk"+window.location.pathname)
 window.location.href = "https://www.duncanritchie.co.uk"+window.location.pathname
+window.location.assign("https://www.duncanritchie.co.uk"+window.location.pathname)
