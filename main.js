232c6e7b52d4cc25e6fef1a056996b6ce7138706
Duncan Ritchie <duncanritchie@btinternet.com>
2019-05-21 11:50:12 +0100

    Uncommented .assign()

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const url = window.location.pathname
 const filename = url.substring(url.lastIndexOf('/'))
 const newUrl = "https://www.duncanritchie.co.uk"+filename
-console.log(newUrl)
 // window.location.replace(newUrl)
 // window.location.href = newUrl
-// window.location.assign(newUrl)
+window.location.assign(newUrl)
