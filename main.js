16d066db4063380c196902e4846e24b09ee91ab2
Duncan Ritchie <duncanritchie@btinternet.com>
2019-05-21 11:33:10 +0100

    Commented out the window.location functions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const url = window.location.pathname;
-const filename = url.substring(url.lastIndexOf('/'));
+const url = window.location.pathname
+const filename = url.substring(url.lastIndexOf('/'))
 const newUrl = "https://www.duncanritchie.co.uk"+filename
-window.location.replace(newUrl)
-window.location.href = newUrl
-window.location.assign(newUrl)
+console.log(newUrl)
+// window.location.replace(newUrl)
+// window.location.href = newUrl
+// window.location.assign(newUrl)
