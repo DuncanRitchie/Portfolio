447aca8990a866eb24947b6505b033987872ac04
Duncan Ritchie <duncanritchie@btinternet.com>
2019-05-21 11:13:12 +0100

    Deleted all but .assign() in main.js

--- main.js
+++ main.js
@@ -1,3 +1,1 @@
-window.location.replace("https://www.duncanritchie.co.uk"+window.location.pathname)
-window.location.href = "https://www.duncanritchie.co.uk"+window.location.pathname
 window.location.assign("https://www.duncanritchie.co.uk"+window.location.pathname)
